be778bf616e7fb333392700eccd9cc8af8d6043e
Trouni Tiet <trouni@gmail.com>
2022-05-19 10:44:20 +0900

    Add PgSearch implementation

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -3,15 +3,24 @@ class MoviesController < ApplicationController
   def index
     @movies =
       if params[:q].present?
-        sql_query = <<-SQL
-          movies.title @@ :q
-          OR movies.synopsis @@ :q
-          OR directors.first_name @@ :q
-          OR directors.last_name @@ :q
-        SQL
-        Movie.joins(:director).where(sql_query, q: "%#{params[:q]}%")
+        ## ActiveRecord implementation
+        #
+        # sql_query = <<-SQL
+        #   movies.title @@ :q
+        #   OR movies.synopsis @@ :q
+        #   OR directors.first_name @@ :q
+        #   OR directors.last_name @@ :q
+        # SQL
+        # Movie.joins(:director).where(sql_query, q: "%#{params[:q]}%")
+
+        ## PgSearch implementation
+        #
+        Movie.global_search(params[:q])
       else
         Movie.all
       end
+
+    ## If filtering further by location, add a new location input, then filter the records further with:
+    # @movies = @movies.near(params[:location]) if params[:location]
   end
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,11 @@
 class Movie < ApplicationRecord
   belongs_to :director
+
+  include PgSearch::Model
+  multisearchable against: [:title, :synopsis]
+
+  pg_search_scope :global_search,
+                  against: [:title, :synopsis],
+                  associated_against: { director: [:first_name, :last_name] },
+                  using: { tsearch: { prefix: true } }
 end
